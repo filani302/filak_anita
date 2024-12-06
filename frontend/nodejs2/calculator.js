7d39e540463690b8535a34fd22a45fd0c03021ab
filani302 <filani302@hengersor.hu>
2024-12-06 11:19:14 +0100

    nodejs2
    server

---
+++ frontend/nodejs2/calculator.js
@@ -0,0 +1,7 @@
+//function(_dirname_filename, module, require, exports){
+function add(num1, num2){
+    return num1 * num2;
+}
+module.exports=add;
+
+//}
--- frontend/nodejs2/index.js
+++ frontend/nodejs2/index.js
@@ -1,9 +1,28 @@
+//absolut es a relativ utvonal!!!!!
+//console.log('ok');
+
+
 //wrapper function
 //function(){
+
 
 
 console.log('_dirname');
 console.log('_filename');
 
-//}
+const add = require('./calculator.js'); 
 
+console.log(add(2, 3));
+
+
+require('http');
+
+
+const server = Http2ServerRequest.createServer(function(req, res){
+    res.writeHead(200);
+    res.end('Hello World');
+});
+
+
+server.listener(8080);
+//}
