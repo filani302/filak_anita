adfa9b5bdf9c51c416d69e48c8663d406bfb0286
filani302 <filani302@hengersor.hu>
2025-02-05 09:35:14 +0100

    laravel
    ..

---
+++ mestermunka/ReAnBeauty/reanbeauty/database/migrations/2025_02_05_080358_users.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id('user_id'); // Egyedi ID (primary key)
+            $table->string('username', 50);
+            $table->integer('role'); // Szerepkör
+            $table->string('email', 250)->unique();
+            $table->string('phone_number', 12);
+            $table->string('password', 12);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('modified_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users'); // Visszavonás esetén törli a táblát
+    }
+};
