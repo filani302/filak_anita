a25d1687f5bce630439f7896c4f20d71ac75b727
petreb904 <82363199+petreb904@users.noreply.github.com>
2025-02-21 12:47:54 +0100

    oldalak elkeszitese
    2025.02.21

--- mestermunka/ReAnBeauty/reanbeauty/routes/web.php
+++ mestermunka/ReAnBeauty/reanbeauty/routes/web.php
@@ -12,7 +12,49 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
+ 
+Route::get('/custom-css', function () {
+    $path = public_path('css/app.css');
+    if (File::exists($path)) {
+        return Response::file($path, ['Content-Type' => 'text/css']);
+    } else {
+        abort(404);
+    }
+});
+Route::get('/welcome', function () {
     return view('welcome');
+});
+
+Route::get('/header', function () {
+    return view('header');
+});
+
+Route::get('/footer', function () {
+    return view('footer');
+});
+Route::get('/', function () {
+    return view('fooldal');
+});
+
+Route::get('/termekek', function () {
+    return view('termekek');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/registration', function () {
+    return view('registration');
+});
+
+Route::get('/rutinok', function () {
+    return view('rutinok');
+});
+
+Route::get('/rutinfeltoltesek', function () {
+    return view('rutinfeltoltesek');
+});
+Route::get('/profil', function () {
+    return view('profil');
 });
