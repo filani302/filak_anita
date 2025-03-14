5c69ca560eb1d20c66c1c0d31ec4716b0bb1c655
petreb904 <82363199+petreb904@users.noreply.github.com>
2025-03-14 11:24:57 +0100

    rutin/termek-kinezet

--- mestermunka/ReAnBeauty/reanbeauty/resources/views/rutinok.blade.php
+++ mestermunka/ReAnBeauty/reanbeauty/resources/views/rutinok.blade.php
@@ -7,10 +7,11 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>ReAnBeauty - Rutinok</title>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    
 </head>
-<body class="rutin-body"> <!-- Itt adtuk hozzá az osztályt -->
-<nav class="navbar navbar-expand-lg bg-dark shadow-sm">
+<body class="rutin-body">
+    
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg bg-dark shadow-sm">
         <div class="container">
             <a class="navbar-brand text-light fs-4" href="{{ url('/welcome') }}">
                 <img src="/img/ReAnLogoo.png" class="ReAnLogoo" alt="Logo" width="50"> ReAnBeauty
@@ -29,6 +30,7 @@
         </div>
     </nav>
 
+    <!-- Offcanvas Menü -->
     <div class="offcanvas offcanvas-end d-lg-none" tabindex="-1" id="offcanvasNav">
         <div class="offcanvas-header">
             <h5 class="offcanvas-title">Menü</h5>
@@ -36,8 +38,7 @@
         </div>
         <div class="offcanvas-body">
             <ul class="navbar-nav">
-            <li class="nav-item"><a class="nav-link" href="#">Főoldal</a></li>
-            <li class="nav-item"><a class="nav-link" href="{{ url('/welcome') }}">Főoldal</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ url('/welcome') }}">Főoldal</a></li>
                 <li class="nav-item"><a class="nav-link" href="{{ url('/termekek') }}">Termékek</a></li>
                 <li class="nav-item"><a class="nav-link" href="{{ url('/rutinok') }}">Rutinok</a></li>
                 <li class="nav-item"><a class="nav-link" href="{{ url('/profil') }}">Profil</a></li>
@@ -46,69 +47,65 @@
         </div>
     </div>
 
-    <section class="hero-section2">
-        <div class="hero-content">
-            <h1 id="oszd-meg" class="fw-bolder">✨ Oszd meg aktuális rutinodról a véleményed ✨</h1>
-            <p class="lead text-dark ">Bőrápolás. Hajápolás. Rutinok. Inspiráció.</p>
+    <!-- Hero szekció -->
+    <section class="hero-section2 text-center py-5">
+        <div class="container">
+            <h1 class="fw-bolder">✨ Oszd meg aktuális rutinodról a véleményed ✨</h1>
+            <p class="lead text-dark">Bőrápolás. Hajápolás. Rutinok. Inspiráció.</p>
             <a href="{{ url('/rutinfeltoltesek') }}" class="btn btn-custom me-3">Megosztom a rutinom</a>
         </div>
     </section>
 
-    
+    <!-- Rutinok listája -->
+    <div class="container my-5">
+        <h1 class="text-center mb-4">Rutinok</h1>
 
-  <center><h1>Rutinok</h1></center>  
-
-    <div class="container">
-    <!-- Nagy doboz -->
-    <div class="content-box">
-        <div class="row">
-            <!-- Bal oldal: szövegek -->
-            <div class="col-md-8">
-                <p><strong>Cím vagy termék neve</strong></p>
-                <p>Termékeírás
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                    Maiores placeat quam neque hic impedit distinctio beatae 
-                    magnam incidunt at nemo numquam eveniet necessitatibus aliquam 
-                    dolores perspiciatis, dolore consectetur quidem dolorum?</p>
-                </p>
+        <!-- Rutin 1 -->
+        <div class="content-box p-4 shadow-sm rounded bg-light mb-4">
+            <div class="row align-items-center">
+                <div class="col-12 col-md-8">
+                    <h5><strong>Cím vagy termék neve</strong></h5>
+                    <p class="mb-2">Termékleírás</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                        Maiores placeat quam neque hic impedit distinctio beatae 
+                        magnam incidunt at nemo numquam eveniet necessitatibus aliquam 
+                        dolores perspiciatis, dolore consectetur quidem dolorum?</p>
+                </div>
+                <div class="col-12 col-md-4 text-center">
+                    <img src="kephelye.jpg" alt="Rutin kép" class="img-fluid rounded">
+                </div>
             </div>
-            <!-- Jobb oldal: Kép helye -->
-            <div class="col-md-4">
-            <img src="kephelye.jpg" alt=""> 
-            <p>kép</p>         
-         </div>
-         <!-- Ikonok (Kedvelés, Kedvencek, Komment) -->
-    
-        <i class="fas fa-heart">like</i>  <!-- Kedvelés -->
-        <i class="fas fa-star"></i>   <!-- Kedvencek -->
-        <i class="fas fa-comment"></i> <!-- Komment -->      
-    </div>
+            <div class="mt-3 d-flex justify-content-start gap-3">
+                <i class="fas fa-heart"> ❤️ Like</i>
+                <i class="fas fa-star"> ⭐ Kedvencek</i>
+                <i class="fas fa-comment"> 💬 Komment</i>
+            </div>
+        </div>
+
+        <!-- Rutin 2 -->
+        <div class="content-box p-4 shadow-sm rounded bg-light mb-4">
+            <div class="row align-items-center">
+                <div class="col-12 col-md-8">
+                    <h5><strong>Cím vagy termék neve</strong></h5>
+                    <p class="mb-2">Termékleírás</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                        Maiores placeat quam neque hic impedit distinctio beatae 
+                        magnam incidunt at nemo numquam eveniet necessitatibus aliquam 
+                        dolores perspiciatis, dolore consectetur quidem dolorum?</p>
+                </div>
+                <div class="col-12 col-md-4 text-center">
+                    <img src="kephelye.jpg" alt="Rutin kép" class="img-fluid rounded">
+                </div>
+            </div>
+            <div class="mt-3 d-flex justify-content-start gap-3">
+                <i class="fas fa-heart"> ❤️ Like</i>
+                <i class="fas fa-star"> ⭐ Kedvencek</i>
+                <i class="fas fa-comment"> 💬 Komment</i>
+            </div>
+        </div>
     </div>
 
-    <div class="content-box">
-        <div class="row">
-            <!-- Bal oldal: szövegek -->
-            <div class="col-md-8">
-                <p><strong>Cím vagy termék neve</strong></p>
-                <p>Termékleírás
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                    Maiores placeat quam neque hic impedit distinctio beatae 
-                    magnam incidunt at nemo numquam eveniet necessitatibus aliquam 
-                    dolores perspiciatis, dolore consectetur quidem dolorum?</p>
-                </p>
-            </div>
-            <!-- Jobb oldal: Kép helye -->
-            <div class="col-md-4">
-            <img src="kephelye.jpg" alt="">  
-            <p>kép</p>        
-         </div>
-         <!-- Ikonok (Kedvelés, Kedvencek, Komment) -->
-    
-        <i class="fas fa-heart">like</i>  <!-- Kedvelés -->
-        <i class="fas fa-star"></i>   <!-- Kedvencek -->
-        <i class="fas fa-comment"></i> <!-- Komment -->      
-    </div>
-    </div>
-</div>
+    <!-- FontAwesome ikonokhoz -->
+
 </body>
-</head>
+</html>
--- mestermunka/ReAnBeauty/reanbeauty/resources/views/termekek.blade.php
+++ mestermunka/ReAnBeauty/reanbeauty/resources/views/termekek.blade.php
@@ -5,17 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <title>ReAnBeauty - Termékoldal</title>
+    <title>ReAnBeauty - Termékek</title>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-   
 </head>
 <body class="termekek-body">
+    
+    <!-- Navbar -->
     <nav class="navbar navbar-expand-lg bg-dark shadow-sm">
         <div class="container">
             <a class="navbar-brand text-light fs-4" href="{{ url('/welcome') }}">
                 <img src="/img/ReAnLogoo.png" class="ReAnLogoo" alt="Logo" width="50"> ReAnBeauty
             </a>
-            <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNav">
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNav">
                 <span class="navbar-toggler-icon bg-light"></span>
             </button>
             <div class="collapse navbar-collapse d-none d-lg-block">
@@ -24,20 +25,12 @@
                     <li class="nav-item"><a class="nav-link text-light" href="{{ url('/termekek') }}">Termékek</a></li>
                     <li class="nav-item"><a class="nav-link text-light" href="{{ url('/rutinok') }}">Rutinok</a></li>
                     <li class="nav-item"><a class="nav-link text-light" href="{{ url('/profil') }}">Profil</a></li>
- 
                 </ul>
             </div>
         </div>
     </nav>
 
-    <section class="hero-section2">
-        <div class="hero-content">
-            <h1 class="fw-bold">✨ Oszd meg aktuális termékedről a véleményed ✨</h1>
-            <p class="lead">Bőrápolás. Hajápolás. Rutinok. Inspiráció.</p>
-            <a href="{{ url('/termekfeltoltesek') }}" class="btn btn-custom me-3">Megosztom a termékem</a>
-        </div>
-    </section>
- 
+    <!-- Offcanvas Menü (Mobilhoz) -->
     <div class="offcanvas offcanvas-end d-lg-none" tabindex="-1" id="offcanvasNav">
         <div class="offcanvas-header">
             <h5 class="offcanvas-title">Menü</h5>
@@ -52,19 +45,81 @@
             </ul>
         </div>
     </div>
-    
-    
 
-    <center><h1>Termékek</h1></center>
-    <div class="termek-card">
-    <div class="card" style="width: 30rem;">
-  <img src="/img/ReAnLogoo.png" class="card-img-top" alt="...">
-  <div class="card-body">
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
+    <!-- Hero szekció -->
+    <section class="hero-section2 text-center py-5">
+        <div class="container">
+            <h1 class="fw-bold">✨ Oszd meg a véleményed a termékekről ✨</h1>
+            <p class="lead">Bőrápolás. Hajápolás. Rutinok. Inspiráció.</p>
+            <a href="{{ url('/termekfeltoltesek') }}" class="btn btn-custom me-3">Megosztom a termékem</a>
+        </div>
+    </section>
 
+    <!-- Termékek listája -->
+    <div class="container my-5">
+        <h1 class="text-center mb-4">Termékek</h1>
 
-</div>
+        <!-- Termék 1 -->
+        <div class="content-box p-4 shadow-sm rounded bg-light mb-4">
+            <div class="row align-items-center">
+                <div class="col-12 col-md-4 text-center">
+                    <img src="/img/ReAnLogoo.png" alt="Termék kép" class="img-fluid rounded">
+                </div>
+                <div class="col-12 col-md-8">
+                    <h5><strong>Termék neve</strong></h5>
+                    <p class="mb-2">Termékleírás</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores placeat quam neque hic impedit distinctio beatae.</p>
+                </div>
+            </div>
+            <div class="mt-3 d-flex justify-content-start gap-3">
+                <i class="fas fa-heart"> ❤️ Like</i>
+                <i class="fas fa-star"> ⭐ Kedvencek</i>
+                <i class="fas fa-comment"> 💬 Komment</i>
+            </div>
+        </div>
+
+        <!-- Termék 2 -->
+        <div class="content-box p-4 shadow-sm rounded bg-light mb-4">
+            <div class="row align-items-center">
+                <div class="col-12 col-md-4 text-center">
+                    <img src="/img/ReAnLogoo.png" alt="Termék kép" class="img-fluid rounded">
+                </div>
+                <div class="col-12 col-md-8">
+                    <h5><strong>Termék neve</strong></h5>
+                    <p class="mb-2">Termékleírás</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores placeat quam neque hic impedit distinctio beatae.</p>
+                </div>
+            </div>
+            <div class="mt-3 d-flex justify-content-start gap-3">
+                <i class="fas fa-heart"> ❤️ Like</i>
+                <i class="fas fa-star"> ⭐ Kedvencek</i>
+                <i class="fas fa-comment"> 💬 Komment</i>
+            </div>
+        </div>
+
+        <!-- Termék 3 -->
+        <div class="content-box p-4 shadow-sm rounded bg-light mb-4">
+            <div class="row align-items-center">
+                <div class="col-12 col-md-4 text-center">
+                    <img src="/img/ReAnLogoo.png" alt="Termék kép" class="img-fluid rounded">
+                </div>
+                <div class="col-12 col-md-8">
+                    <h5><strong>Termék neve</strong></h5>
+                    <p class="mb-2">Termékleírás</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores placeat quam neque hic impedit distinctio beatae.</p>
+                </div>
+            </div>
+            <div class="mt-3 d-flex justify-content-start gap-3">
+                <i class="fas fa-heart"> ❤️ Like</i>
+                <i class="fas fa-star"> ⭐ Kedvencek</i>
+                <i class="fas fa-comment"> 💬 Komment</i>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- FontAwesome ikonokhoz -->
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+
 </body>
-</head>
+</html>
